32e436eed83a683af0232cd96ae213970430052f
joungeun <lje0818ovo@naver.com>
2020-10-25 23:19:10 +0900

    constructor and Separation of definitions and declarations of member functions

---
+++ C++/06)constructor&memberFunction.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Test {
+public:
+    Test():data(17),data2("멤버 함수 정의\n"){} // 생성자 초기화 목록
+    int data; 
+    string data2;
+
+    void Print(); //멤버 함수 선언
+};
+
+void Test::Print() {
+    cout << "클래스 외부에 분리된 ";
+    cout << data2;
+}
+
+int main()
+{
+    Test test;
+    test.Print();
+    return 0;
+}
